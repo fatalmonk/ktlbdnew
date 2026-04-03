21c50b2283ffa05b425220743947b9bfd3f3b690
fatalmonk <anwar.alvi18@gmail.com>
2026-04-04 00:43:21 +0600

    chore: remove obsolete archive directory and unused type definition

--- apps/web/src/types/react-img.d.ts
+++
@@ -1,8 +0,0 @@
-import "react";
-
-/** React 18 warns on `fetchPriority`; HTML uses lowercase `fetchpriority`. */
-declare module "react" {
-  interface ImgHTMLAttributes {
-    fetchpriority?: "high" | "low" | "auto";
-  }
-}
